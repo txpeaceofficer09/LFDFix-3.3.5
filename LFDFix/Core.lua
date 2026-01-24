a3d9c3aa216f09988501fba4c7fbac277881e034
txpeaceofficer09 <james.mccaughey@gmail.com>
2026-01-24 12:01:56 -0600

    Make Ascended I the default queue.

--- LFDFix/Core.lua
+++ LFDFix/Core.lua
@@ -3,23 +3,16 @@ local lfdOpen = nil
 
 function LFDDungeonReadyDialogReward_SetReward(button, dungeonID, rewardIndex)
 	local name, texturePath, quantity = GetLFGDungeonRewardInfo(dungeonID, rewardIndex);
-	if type(texturePath) ~= nil and texturePath:find("Interface\\Icons\\", 1, true) == 1 then	--Otherwise, we may be waiting on the item data to come from the server.
-		--print(button:GetName(), dungeonID, rewardIndex, name, texturePath, quantity)
+	if type(texturePath) ~= nil and texturePath:find("Interface\\Icons\\", 1, true) == 1 then
 		SetPortraitToTexture(button.texture, texturePath);
 	end
 	button.rewardID = rewardIndex;
 	button:Show();
 end
 
---f:SetScript("OnEvent", OnEvent)
-
---f:RegisterEvent("ADDON_LOADED")
-
-
 LFDParentFrame:HookScript("OnShow", function()
     local mode = GetLFGMode()
     if not mode then
-    	--LFDQueueFrame_SetType(260)
         lfdOpen = GetTime()
     end
 end)
