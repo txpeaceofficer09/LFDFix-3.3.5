76dfdb993a3930b7ceab1f2568ab6581dfd697da
txpeaceofficer09 <james.mccaughey@gmail.com>
2026-01-31 01:01:49 -0600

    ...

--- LFDFix/Core.lua
+++ LFDFix/Core.lua
@@ -3,6 +3,7 @@ local lfdOpen = nil
 
 function LFDDungeonReadyDialogReward_SetReward(button, dungeonID, rewardIndex)
 	local name, texturePath, quantity = GetLFGDungeonRewardInfo(dungeonID, rewardIndex);
+	print(name, texturePath, quantity, dungeonID, rewardIndex, button:GetName())
 	if type(texturePath) ~= nil and texturePath:find("Interface\\Icons\\", 1, true) == 1 then
 		SetPortraitToTexture(button.texture, texturePath);
 	end
@@ -12,7 +13,7 @@ end
 
 LFDParentFrame:HookScript("OnShow", function()
     local mode = GetLFGMode()
-    if not mode then
+    if not mode or mode == "abandonedInDungeon" then
         lfdOpen = GetTime()
         f:Show()
     end
@@ -21,7 +22,13 @@ end)
 local function OnUpdate(self, elapsed)
 	if lfdOpen ~= nil then
 		if GetTime() - lfdOpen >= 0.2 then
-			LFDQueueFrame_SetType(260)
+			local dungeonComplete = false
+			local mode = GetLFGMode()
+
+			if not mode or dungeonComplete or mode == "abandonedInDungeon" then
+				LFDQueueFrame_SetType(260)
+			end
+
 			lfdOpen = nil
 			self:Hide()
 		end
